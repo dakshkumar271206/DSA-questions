bb647da8683f686583143fe6214a6e213d8842a0
Daksh Kumar <dakshkumar291186@gmail.com>
2026-07-21 12:58:27 +0530

    Time: 76 ms (43.6%), Space: 13.5 MB (12.97%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def addToArrayForm(self, num, k):
+        r = ''
+        re = []
+        for _ in range(len(num)):
+            r = r + str(num[_])
+        r = str(int(r) + k)
+        for _ in r:
+            re.append(int(_))
+        return re
+        """
+        :type num: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        
