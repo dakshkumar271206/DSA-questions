0b88c28665fa097d338b02e3d083a4a7bf58fc26
Daksh Kumar <dakshkumar291186@gmail.com>
2026-08-14 11:18:17 +0530

    Time: 1 ms (21.54%), Space: 12.3 MB (64.17%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -6,8 +6,4 @@ class Solution(object):
             for j in range(1, i):
                 row[j] = triangle[i-1][j-1] + triangle[i-1][j]
             triangle.append(row)
-        max_width = numRows * 4 
-        for row in triangle:
-            row_string = "   ".join(map(str, row))
-            print(row_string.center(max_width))
         return triangle
